b60bb4b830d138eab8006c6417d64d1b42c16317
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-02-16 17:17:17 +0530

    Basic Account Setup Done

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+async function authMiddleware(req, res, next) {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized. No token provided.",
+      status: "failed",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findById(decoded.id);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized. Invalid token.",
+      status: "failed",
+    });
+  }
+}
+
+module.exports = { authMiddleware };
